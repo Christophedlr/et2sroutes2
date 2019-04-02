94192767ae3111cb3ce36ac597b94250777681f8
Christophe Daloz - De Los Rios <christophedlr@gmail.com>
2019-04-02 18:54:32 +0200

    General
    - Add: layout.html.twig
    - Add: header.html.twig
    - Add: footer.html.twig
    - Add: Index controller
    - Add: index.html.twig Index template

---
+++ src/Bundle/Base/Controller/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright (c) 2019 Christophe Daloz - De Los Rios
+ * This code is licensed under MIT license (see LICENSE for details)
+ */
+
+namespace Bundle\Base\Controller;
+
+
+use Kernel\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IndexController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+        return $this->getTemplate()->renderResponse('@Base/index.html.twig');
+    }
+}
