edbf760a3e5c7000a3916ecc1d95219b0ccc62e7
Christophe Daloz - De Los Rios <christophedlr@gmail.com>
2019-04-03 09:58:39 +0200

    Configuration
    - Add: vars

--- config/parameters.php
+++ config/parameters.php
@@ -8,6 +8,7 @@ $injection['parameter'] = [];
 $injection['service'] = [];
 
 $injection['parameter'] = array_merge($injection['parameter'], require_once 'parameters/database.php');
+$injection['parameter'] = array_merge($injection['parameter'], require_once 'parameters/vars.php');
 $injection['service'] = array_merge($injection['service'], require_once 'parameters/objects.php');
 
 return $injection;
