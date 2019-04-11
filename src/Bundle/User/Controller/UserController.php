21e93cf5170909d9f7c2abd98ed3bc7d4512aa45
Christophe Daloz - De Los Rios <christophedlr@gmail.com>
2019-04-11 09:43:25 +0200

    UserBundle
    - Add: start session in Controller
    - Add: add global twig var in Controller
    - Add: getSession in Controller
    - Add: getFlashBag in Controller
    - Add: redirectToRoute in Controller
    - Add: save user and redirect to homepage
    - Add: display flash messages in pages

--- src/Bundle/User/Controller/UserController.php
+++ src/Bundle/User/Controller/UserController.php
@@ -10,32 +10,30 @@ namespace Bundle\User\Controller;
 use Bundle\User\Entity\User;
 use Bundle\User\Validator\RegisterValidator;
 use Kernel\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class UserController extends Controller
 {
     public function registerAction(Request $request)
     {
         $validator = new RegisterValidator($request);
-        var_dump($validator->validate());
-        var_dump($validator->getErrors());
-
-        /*if ($request->request->has('form')) {
+        if ($request->request->has('form') && $validator->validate()) {
             $form = $request->request->get('form');
-
             $user = new User();
             $user
                 ->setLogin($form['login'])
                 ->setMail($form['mail']);
-
-            if ($form['password'] === $form['password_confirm']) {
-                $user->setPlainPassword($form['password']);
-            }
+            $user->setPlainPassword($form['password']);
 
             $em = $this->getEntityManager();
             $em->persist($user);
             $em->flush();
-        }*/
+
+            $this->getFlashBag()->add('success', 'Un e-mail vous a été envoyé avec un lien de confirmation');
+            return $this->redirectToRoute('homepage');
+        }
 
         return $this->getTemplate()->renderResponse('@User/register.html.twig', [
             'errors' => $validator->getErrors(),
